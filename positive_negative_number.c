e4739d0a3107d9032b516c01d9f42b11196528c7
Humphrey Ikhalea <84229187+humphreydev6@users.noreply.github.com>
2023-07-21 06:14:26 +0100

    Adding condional statement task

--- positive_negative_number.c
+++ positive_negative_number.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
 
 int main(void){
-  int age = 30;
+ /* int age = 30;
 
   //check if our variable is less than zero
   if (age < 0)
@@ -11,5 +14,22 @@ int main(void){
   }
   else{
     printf("Your condition is positive");
+  }*/
+  int n;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+
+  if (n > 0){
+    printf("%i is positive\n", n);
   }
+  else if (n < 0){
+    printf("%i is negative\n", n);
+  }
+  else{
+    printf("%i is zero\n", n);
+  }
+
+    return (0);
+
 }
