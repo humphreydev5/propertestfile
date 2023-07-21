4b48b6fbbc39b333f72e75be6a8ff4e75adc5452
Humphrey Ikhalea <84229187+humphreydev6@users.noreply.github.com>
2023-07-21 03:04:11 +0100

    Hello world using c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+  
+  int age;
+  age = 20;
+  printf("Hello World \n");
+  printf("My name is Humphrey and I am %i \n", age);
+  age = 30;
+  printf("My name is Mike and I am %i \n", age);
+
+  return (0);
+}
