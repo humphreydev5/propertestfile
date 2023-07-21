77483356ca10eaa1d9ceacc6fcec9893ab6645d2
Humphrey Ikhalea <84229187+humphreydev6@users.noreply.github.com>
2023-07-21 03:16:30 +0100

    Hello world using c

---
+++ hello1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+  int age;
+  age = 30;
+  int year;
+  year = 1993;
+
+  //write the c program here
+  printf("My age is %d and I was born %d", age, year);
+}
